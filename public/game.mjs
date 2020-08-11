f5d19ff786e272dc9f3b1457fbd05f8444800f44
UmbraSpirits <eternalmillenis@outlook.com>
2020-08-11 15:31:27 +0200

    Created working game with score

--- public/game.mjs
+++ public/game.mjs
@@ -5,6 +5,18 @@ const socket = io();
 const canvas = document.getElementById("game-window");
 const context = canvas.getContext("2d");
 
+const FPS = 60;
+
+const playerRadius = 20;
+const playerColor = "#33ccff";
+var startingPosX = 300;
+var startingPosY = 300;
+
+const collectibleRadius = 5;
+const collectibleColor = "#FFD700";
+var collectPosX = 250;
+var collectPosY = 250;
+
 var movement = {
   up: false,
   down: false,
@@ -42,171 +54,87 @@ document.addEventListener("keyup", keyUpHandler, false);
 socket.emit("newPlayer");
 
 setInterval(function () {
-  socket.emit("movement", movement);
-}, 1000 / 60);
+  socket.emit("playerAction", movement);
+}, 1000 / FPS);
 
-socket.on("gameRunning", function (players) {
-  console.log("The game is running");
-  context.clearRect(0, 0, 800, 600);
-  context.fillStyle = "green";
+socket.on("gameRunning", ({ players, collectible }) => {
+  collectPosX = collectible.x;
+  collectPosY = collectible.y;
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  drawCanvas();
+  drawCollectible();
+
+  var scoreboard = [];
+
   for (var id in players) {
     var player = players[id];
-    context.beginPath();
-    context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
-    context.fill();
+    drawPlayer(player.x, player.y);
+    drawScore(player.x, player.y, player.score);
+    scoreboard.push(player.score);
+
+    if (collissionDetection(player.x, player.y)) {
+      socket.emit("collectibleFound", id);
+    }
+  }
+  scoreboard.sort((a, b) => b - a);
+  for (var id in players) {
+    var player = players[id];
+    var rank = scoreboard.indexOf(player.score) + 1;
+    var totalplayers = scoreboard.length;
+    drawRank(player.x, player.y, rank, totalplayers);
   }
 });
 
-// const WIDTH = 640;
-// const HEIGHT = 480;
+const drawPlayer = (x, y) => {
+  context.beginPath();
+  context.arc(x, y, playerRadius, 0, Math.PI * 2);
+  context.fillStyle = playerColor;
+  context.fill();
+  context.strokeStyle = "rgba(0, 0, 255, 0.5)";
+  context.stroke();
+  context.closePath();
+};
 
-// var x = canvas.width / 2;
-// var y = canvas.height - 30;
+const drawCollectible = () => {
+  context.beginPath();
+  context.arc(collectPosX, collectPosY, collectibleRadius, 0, Math.PI * 2);
+  context.fillStyle = collectibleColor;
+  context.fill();
+  context.strokeStyle = "rgba(0, 0, 255, 0.5)";
+  context.stroke();
+  context.closePath();
+};
 
-// var startingPosX =
-//   Math.floor(Math.random() * (canvas.width - playerRadius)) + playerRadius / 2;
-// var startingPosY =
-//   Math.floor(Math.random() * (canvas.height - playerRadius)) + playerRadius / 2;
+var collissionDetection = (x, y) => {
+  if (
+    x + (playerRadius + collectibleRadius) >= collectPosX &&
+    x - (playerRadius + collectibleRadius) <= collectPosX &&
+    y + (playerRadius + collectibleRadius) >= collectPosY &&
+    y - (playerRadius + collectibleRadius) <= collectPosY
+  ) {
+    return true;
+  }
+  return false;
+};
 
-// var dx = 5;
-// var dy = 5;
+const drawCanvas = () => {
+  context.beginPath();
+  context.fillStyle = "#d3d3d3";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  context.rect(0, 0, canvas.width, canvas.height);
+  context.strokeStyle = "rgba(0, 0, 255, 0.5)";
+  context.stroke();
+  context.closePath();
+};
 
-// var playerRadius = 20;
-// var playerColor = "#33ccff";
+const drawScore = (x, y, score) => {
+  context.font = "16px Arial";
+  context.fillStyle = "#fc03c2";
+  context.fillText("Score: " + score, x - 35, y);
+};
 
-// var collectibleRadius = 5;
-// var collectibleColor = "#FFD700";
-// var collectPosX = Math.floor(Math.random() * canvas.width);
-// var collectPosY = Math.floor(Math.random() * canvas.height);
-
-// var score = 0;
-
-// var movement = {
-//   rightMove: false,
-//   leftMove: false,
-//   upMove: false,
-//   downMove: false,
-// };
-
-//
-
-// const drawPlayer = () => {
-//   context.beginPath();
-//   context.arc(x, y, playerRadius, 0, Math.PI * 2);
-//   context.fillStyle = playerColor;
-//   context.fill();
-//   context.strokeStyle = "rgba(0, 0, 255, 0.5)";
-//   context.stroke();
-//   context.closePath();
-// };
-
-// const drawCollectible = () => {
-//   context.beginPath();
-//   context.arc(collectPosX, collectPosY, collectibleRadius, 0, Math.PI * 2);
-//   context.fillStyle = collectibleColor;
-//   context.fill();
-//   context.strokeStyle = "rgba(0, 0, 255, 0.5)";
-//   context.stroke();
-//   context.closePath();
-// };
-
-// const collissionDetection = () => {
-//   if (
-//     x + (playerRadius + collectibleRadius) >= collectPosX &&
-//     x - (playerRadius + collectibleRadius) <= collectPosX &&
-//     y + (playerRadius + collectibleRadius) >= collectPosY &&
-//     y - (playerRadius + collectibleRadius) <= collectPosY
-//   ) {
-//     console.log(y, collectPosY);
-//     console.log("collided!");
-//     score += 1;
-//     collectPosX =
-//       Math.floor(Math.random() * (canvas.width - collectibleRadius)) +
-//       collectibleRadius / 2;
-//     collectPosY =
-//       Math.floor(Math.random() * (canvas.height - collectibleRadius)) +
-//       collectibleRadius / 2;
-//   }
-// };
-
-// const drawCanvas = () => {
-//   context.beginPath();
-//   context.fillStyle = "#d3d3d3";
-//   context.fillRect(0, 0, WIDTH, HEIGHT);
-//   context.rect(0, 0, WIDTH, HEIGHT);
-//   context.strokeStyle = "rgba(0, 0, 255, 0.5)";
-//   context.stroke();
-//   context.closePath();
-// };
-
-// const drawScore = () => {
-//   context.font = "16px Arial";
-//   context.fillStyle = "#fc03c2";
-//   context.fillText("Score: " + score, 8, 20);
-// };
-
-// const draw = () => {
-//   context.clearRect(0, 0, canvas.width, canvas.height);
-//   drawCanvas();
-//   drawPlayer();
-//   drawCollectible();
-//   collissionDetection();
-//   drawScore();
-//   //   Movement of player
-//   if (rightMove) {
-//     if (x + dx < canvas.width - playerRadius) {
-//       x += dx;
-//     }
-//   } else if (leftMove) {
-//     if (x - dx > playerRadius) {
-//       x -= dx;
-//     }
-//   } else if (upMove) {
-//     if (y - dy > playerRadius) {
-//       y -= dy;
-//     }
-//   } else if (downMove) {
-//     if (y + dy < canvas.height - playerRadius) {
-//       y += dy;
-//     }
-//   }
-// };
-
-// socket.on("init", ({ id }) => {
-//   console.log(`Game started for player ${id}`);
-
-//   const mainPlayer = new Player({
-//     x: startingPosX,
-//     y: startingPosY,
-//     id,
-//   });
-
-//   socket.emit("createdPlayer", mainPlayer);
-
-//   setInterval(draw, 10);
-
-//   document.addEventListener("keydown", keyDownHandler, false);
-//   document.addEventListener("keyup", keyUpHandler, false);
-// });
-
-//
-
-// socket.emit("new player");
-// setInterval(function () {
-//   socket.emit("movement", movement);
-// }, 1000 / 60);
-
-// var canvas = document.getElementById("canvas");
-// canvas.width = 800;
-// canvas.height = 600;
-// var context = canvas.getContext("2d");
-// socket.on("state", function (players) {
-//   context.clearRect(0, 0, 800, 600);
-//   context.fillStyle = "green";
-//   for (var id in players) {
-//     var player = players[id];
-//     context.beginPath();
-//     context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
-//     context.fill();
-//   }
-// });
+const drawRank = (x, y, rank, totalplayers) => {
+  context.font = "16px Arial";
+  context.fillStyle = "#fc03c2";
+  context.fillText(`Rank: ${rank}/${totalplayers}`, x - 35, y - 16);
+};
--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const expect = require("chai");
 const socket = require("socket.io");
+const helmet = require("helmet");
 
 const fccTestingRoutes = require("./routes/fcctesting.js");
 const runner = require("./test-runner.js");
 
 const app = express();
+
+app.use(helmet());
+app.use(helmet.noCache());
+app.use(helmet.hidePoweredBy({ setTo: "PHP 7.4.3" }));
 
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use("/assets", express.static(process.cwd() + "/assets"));
@@ -46,42 +51,79 @@ const server = app.listen(portNum, () => {
   }
 });
 
+// Running the game
 const io = socket(server);
+const FPS = 60;
+const WIDTH = 640;
+const HEIGHT = 480;
 var players = {};
+var movementSpeed = 5;
+var playerRadius = 20;
+var collectibleRadius = 5;
+var collectible = {
+  x:
+    Math.floor(Math.random() * (WIDTH - collectibleRadius)) + collectibleRadius,
+  y:
+    Math.floor(Math.random() * (HEIGHT - collectibleRadius)) +
+    collectibleRadius,
+};
 
 io.sockets.on("connection", (socket) => {
   console.log(`User with ID ${socket.id} has connected`);
 
   socket.on("newPlayer", () => {
     players[socket.id] = {
-      x: 300,
-      y: 300,
+      x: Math.floor(Math.random() * (WIDTH - playerRadius)) + playerRadius,
+      y: Math.floor(Math.random() * (HEIGHT - playerRadius)) + playerRadius,
+      score: 0,
     };
   });
 
-  socket.on("movement", function (data) {
+  socket.on("playerAction", (data) => {
     var player = players[socket.id] || {};
     if (data.left) {
-      player.x -= 5;
+      if (player.x - movementSpeed > playerRadius) {
+        player.x -= movementSpeed;
+      }
     }
     if (data.up) {
-      player.y -= 5;
+      if (player.y - movementSpeed > playerRadius) {
+        player.y -= movementSpeed;
+      }
     }
     if (data.right) {
-      player.x += 5;
+      if (player.x + movementSpeed < 640 - playerRadius) {
+        player.x += movementSpeed;
+      }
     }
     if (data.down) {
-      player.y += 5;
+      if (player.y + movementSpeed < 480 - playerRadius) {
+        player.y += movementSpeed;
+      }
     }
   });
+
+  socket.on("collectibleFound", (playerFound) => {
+    if (socket.id == playerFound) {
+      var player = players[playerFound];
+      (collectible.x =
+        Math.floor(Math.random() * (WIDTH - collectibleRadius)) +
+        collectibleRadius),
+        (collectible.y =
+          Math.floor(Math.random() * (HEIGHT - collectibleRadius)) +
+          collectibleRadius),
+        (players[playerFound].score += 1);
+    }
+  });
+
   socket.on("disconnect", () => {
     console.log(`A user with ID: ${socket.id} has disconnected`);
     delete players[socket.id];
   });
 });
 
-setInterval(function () {
-  io.sockets.emit("gameRunning", players);
-}, 1000 / 60);
+setInterval(() => {
+  io.sockets.emit("gameRunning", { players, collectible });
+}, 1000 / FPS);
 
 module.exports = app; // For testing
